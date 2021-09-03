1ddf6d00482ecd644d0e6fd48f267b5703368eb3
FancySlime <gorvin.phey@gmail.com>
2021-09-03 13:46:00 -0600

    Exctracted Settings Class

--- Program.cs
+++ Program.cs
@@ -108,25 +108,6 @@ namespace SaltyBetter
                 Console.Read();
             }
         }
-
-
-        class Settings
-        {
-            public string email { get; set; }
-            public string password { get; set; }
-            public int waitTime { get; set; }
-            public string chromeDriverPath { get; set; }
-            public int betAmount { get; set; }
-
-            public Settings()
-            {
-                email = "";
-                password = "";
-                waitTime = 1000;
-                chromeDriverPath = "";
-                betAmount = 1;
-            }
-        }
     }
 }
 
