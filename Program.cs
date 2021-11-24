a4b694f3efd9f44e8a0bbbbd0570bfee7e11326e
FuncoSlam <gorvin.phey@gmail.com>
2021-11-23 18:44:16 -0600

    Update to chromedriver 96

--- Program.cs
+++ Program.cs
@@ -10,154 +10,154 @@ namespace SaltyBetter;
 
 class Program
 {
-    public Settings settings = new();
+	public Settings settings = new();
 
-    public IWebDriver driver;
+	public IWebDriver driver;
 
-    readonly string jsonFilePath = "./SaltyBetterSettings.json";
+	readonly string jsonFilePath = "./SaltyBetterSettings.json";
 
-    readonly Random random = new();
-    bool hasBet = false;
-    public bool syncRefresh = false;
+	readonly Random random = new();
+	bool hasBet = false;
+	public bool syncRefresh = false;
 
-    public async Task Run()
-    {
+	public async Task Run()
+	{
 
-        // CREATE OR LOAD SETTINGS FILE //
+		// CREATE OR LOAD SETTINGS FILE //
 
-        string jsonFilePath = "./SaltyBetterSettings.json";
-        if (File.Exists(jsonFilePath))
-        {
-            settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(jsonFilePath));
-        }
-        else
-        {
-            UpdateSettingsJson();
-        }
+		string jsonFilePath = "./SaltyBetterSettings.json";
+		if (File.Exists(jsonFilePath))
+		{
+			settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(jsonFilePath));
+		}
+		else
+		{
+			UpdateSettingsJson();
+		}
 
-        // CREATE CHROME DRIVER AND OPEN SALTYBET LOGIN PAGE //
+		// CREATE CHROME DRIVER AND OPEN SALTYBET LOGIN PAGE //
 
-        if (File.Exists("./ChromeDriver.exe"))
-        {
-            ChromeOptions options = new();
-            options.AddArgument("--silent");
-            options.AddArgument("--log-level=3");
-            driver = new ChromeDriver(".", options)
-            {
-                Url = "https://www.saltybet.com/authenticate?signin=1"
-            };
-        }
-        else
-        {
-            PromptToEditSettingsFile();
-            return;
-        }
+		if (File.Exists("./ChromeDriver.exe"))
+		{
+			ChromeOptions options = new();
+			options.AddArgument("--silent");
+			options.AddArgument("--log-level=3");
+			driver = new ChromeDriver(".", options)
+			{
+				Url = "https://www.saltybet.com/authenticate?signin=1"
+			};
+		}
+		else
+		{
+			PromptToEditSettingsFile();
+			return;
+		}
 
-        // LOGIN PROCESS //
+		// LOGIN PROCESS //
 
-        LoginInfo loginInfo = new(settings);
-        loginInfo.Login(driver);
-        ExitIfDriverOffSaltyBet();
-        Console.Clear();
+		LoginInfo loginInfo = new(settings);
+		loginInfo.Login(driver);
+		ExitIfDriverOffSaltyBet();
+		Console.Clear();
 
-        // COLLECT NECESARY ELEMENTS //
+		// COLLECT NECESARY ELEMENTS //
 
-        SaltyWebElements webElements = new(driver);
+		SaltyWebElements webElements = new(driver);
 
-        // BEGIN ASYNCHRONOUSLY RECIEVING INPUTS //
+		// BEGIN ASYNCHRONOUSLY RECIEVING INPUTS //
 
-        Task task = ProcessInputAsync();
+		Task task = ProcessInputAsync();
 
-        // EVERY 'waitTime' ms, IF BETTING IS AVAILABLE AND HASN'T BEEN DONE ALREADY, BET 'betAmount' SALT ON RANDOM SIDE //
+		// EVERY 'waitTime' ms, IF BETTING IS AVAILABLE AND HASN'T BEEN DONE ALREADY, BET 'betAmount' SALT ON RANDOM SIDE //
 
-        while (true)
-        {
-            if (webElements.wagerField.Displayed)
-            {
-                if (!hasBet)
-                {
-                    webElements.wagerField.Clear();
-                    webElements.wagerField.SendKeys(settings.BetAmount.ToString());
-                    webElements.buttons[random.Next(webElements.buttons.Count)].Click();
-                    hasBet = true;
-                }
-            }
-            else
-            {
-                hasBet = false;
-            }
+		while (true)
+		{
+			if (webElements.wagerField.Displayed)
+			{
+				if (!hasBet)
+				{
+					webElements.wagerField.Clear();
+					webElements.wagerField.SendKeys(settings.BetAmount.ToString());
+					webElements.buttons[random.Next(webElements.buttons.Count)].Click();
+					hasBet = true;
+				}
+			}
+			else
+			{
+				hasBet = false;
+			}
 
-            await Task.Run(() => Thread.Sleep(settings.WaitTime));
+			await Task.Run(() => Thread.Sleep(settings.WaitTime));
 
-            if (syncRefresh)
-            {
-                Refresh(webElements);
-                syncRefresh = !syncRefresh;
-            }
+			if (syncRefresh)
+			{
+				Refresh(webElements);
+				syncRefresh = !syncRefresh;
+			}
 
-            ExitIfDriverOffSaltyBet();
-        }
-    }
+			ExitIfDriverOffSaltyBet();
+		}
+	}
 
-    private void Refresh(SaltyWebElements webElements)
-    {
-        driver.Navigate().Refresh();
-        Thread.Sleep(1000); // Needed to prevent errors collecting elements before the page has refreshed. 1s may be overkill.
-        webElements.CollectElements(driver);
-    }
+	private void Refresh(SaltyWebElements webElements)
+	{
+		driver.Navigate().Refresh();
+		Thread.Sleep(1000); // Needed to prevent errors collecting elements before the page has refreshed. 1s may be overkill.
+		webElements.CollectElements(driver);
+	}
 
-    public void ExitIfDriverOffSaltyBet()
-    {
-        try
-        {
-            driver.FindElement(By.Id("sbettorswrapper"));
-        }
-        catch
-        {
-            driver.Quit();
-            Environment.Exit(0);
-        }
-    }
+	public void ExitIfDriverOffSaltyBet()
+	{
+		try
+		{
+			driver.FindElement(By.Id("sbettorswrapper"));
+		}
+		catch
+		{
+			driver.Quit();
+			Environment.Exit(0);
+		}
+	}
 
-    static string[] UserPrompt()
-    {
-        Console.Write(">>> ");
-        return Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-    }
+	static string[] UserPrompt()
+	{
+		Console.Write(">>> ");
+		return Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+	}
 
-    public void UpdateSettingsJson()
-    {
-        JsonSerializerOptions jsonSerializerOptions = new();
-        jsonSerializerOptions.WriteIndented = true;
-        File.WriteAllText(jsonFilePath, JsonSerializer.Serialize<Settings>(settings, jsonSerializerOptions));
-    }
+	public void UpdateSettingsJson()
+	{
+		JsonSerializerOptions jsonSerializerOptions = new();
+		jsonSerializerOptions.WriteIndented = true;
+		File.WriteAllText(jsonFilePath, JsonSerializer.Serialize<Settings>(settings, jsonSerializerOptions));
+	}
 
-    static void PromptToEditSettingsFile()
-    {
-        Console.Write(
-            "\n\nAn error likely related to SaltyBetterSettings.json occured." +
-            "\nFill it out with your SaltyBet login details." +
-            "\n\nPress any key to continue...");
-        Console.Read();
-    }
+	static void PromptToEditSettingsFile()
+	{
+		Console.Write(
+			"\n\nAn error likely related to SaltyBetterSettings.json occured." +
+			"\nFill it out with your SaltyBet login details." +
+			"\n\nPress any key to continue...");
+		Console.Read();
+	}
 
-    async Task ProcessInputAsync()
-    {
-        InputParser inputParser = new(this);
+	async Task ProcessInputAsync()
+	{
+		InputParser inputParser = new(this);
 
-        while (true)
-        {
-            string[] input = await Task.Run(() => UserPrompt());
+		while (true)
+		{
+			string[] input = await Task.Run(() => UserPrompt());
 
-            inputParser.Parse(input);
+			inputParser.Parse(input);
 
-            Console.WriteLine(LineBreak("-".ToCharArray()[0]));
-        }
-    }
+			Console.WriteLine(LineBreak("-".ToCharArray()[0]));
+		}
+	}
 
-    static string LineBreak(char lineBreakChar)
-    {
-        string tabs = new(lineBreakChar, Console.BufferWidth);
-        return $"{tabs}\n";
-    }
+	static string LineBreak(char lineBreakChar)
+	{
+		string tabs = new(lineBreakChar, Console.BufferWidth);
+		return $"{tabs}\n";
+	}
 }
