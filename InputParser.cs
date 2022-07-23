f39dec728e8489f165ebdcd318c03e9e6fb37ceb
FuncoSlam <gorvin.phey@gmail.com>
2022-07-23 05:26:41 -0600

    Changed ViewBookmarksCommand() format string

--- InputParser.cs
+++ InputParser.cs
@@ -145,7 +145,7 @@ class InputParser
     private void ViewBookmarksCommand()
     {
         int currentSalt = program.GetCurrentSalt();
-        const string formatString = "${0,-10} {1,-22} {2}";
+        const string formatString = "${0,-10} {1,-10} {2}";
 
         foreach (Bookmark bookmark in bookmarks)
         {
